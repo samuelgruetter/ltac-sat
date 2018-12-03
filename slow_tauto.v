3dd42fd39df9db2ccd2a879ba5d5b3c7578a0cda
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-12-03 13:42:17 -0500

    tauto takes 40s for a problem which z3 solves in 15ms
    but the comparison is unfair because the problem is taken from the
    SMT world and not from a real Coq example

---
+++ slow_tauto.v
@@ -0,0 +1,29 @@
+Definition mygoal :=
+  forall x1 x2 x3 x4 : Prop,
+    x1 \/ x2 \/ ~ x3 ->
+    ~ x1 \/ ~ x2 \/ x3 ->
+    x2 \/ x3 \/ ~ x4 ->
+    ~ x2 \/ ~ x3 \/ x4 ->
+    x1 \/ x3 \/ x4 ->
+    ~ x1 \/ ~ x3 \/ ~ x4 ->
+    ~ x1 \/ x2 \/ x4 ->
+    x1 \/ ~ x2 \/ ~ x4 ->
+    False.
+
+Goal mygoal.
+Proof.
+  unfold mygoal.
+  intros *. intros H1 H2 H3 H4 H5 H6 H7 H8.
+  assert (H9: x1 \/ x2) by (clear -H1 H3 H5; tauto).
+  assert (H10: x1) by (clear -H9 H4 H5 H8; tauto).
+  assert (H11: x2) by (clear -H9 H3 H6 H7; tauto).
+  assert (H12: False) by (clear -H10 H11 H2 H4 H6; tauto).
+  exact H12.
+Qed.
+
+Goal mygoal.
+Proof.
+  unfold mygoal.
+  intros *. intros H1 H2 H3 H4 H5 H6 H7 H8.
+  Time tauto. (* Finished transaction in 40.578 secs (40.323u,0.052s) (successful) *)
+Qed.
